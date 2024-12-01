959b8254fbad3758566d836dd4a05574e3df7d7c
timoreo <contact@timoreo.fr>
2024-12-01 18:40:25 +0100

    First day

---
+++ day1-p1.py
@@ -0,0 +1,22 @@
+
+def main():
+    f = open('day1-input.txt')
+    left_list = []
+    right_list = []
+    total = 0
+    for l in f.readlines():
+        ar = l.split('   ')
+        if len(ar) != 2:
+            continue
+        left_list.append(int(ar[0]))
+        right_list.append(int(ar[1]))
+    left_list = sorted(left_list)
+    right_list = sorted(right_list)
+    while len(left_list) != 0:
+        total += abs(left_list.pop() - right_list.pop())
+    print(total)
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ day1-p2.py
@@ -0,0 +1,24 @@
+from collections import Counter
+
+def main():
+    f = open('day1-input.txt')
+    left_list = []
+    right_list = []
+    total = 0
+    for l in f.readlines():
+        ar = l.split('   ')
+        if len(ar) != 2:
+            continue
+        left_list.append(int(ar[0]))
+        right_list.append(int(ar[1]))
+    total_dict = Counter(right_list)
+    while len(left_list) != 0:
+        to_search = left_list.pop()
+        if to_search in total_dict:
+            total += to_search * total_dict[to_search]
+    print(total)
+
+
+
+if __name__ == '__main__':
+    main()
