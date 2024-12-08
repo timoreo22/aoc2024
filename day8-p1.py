87a93f7aedd7d36e1259a2373230ebcc2a16d2ca
timoreo <contact@timoreo.fr>
2024-12-08 06:54:27 +0100

    Day 8 !

---
+++ day8-p1.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+
+
+def main():
+    f = open('day8-input.txt', 'r')
+    antennas = defaultdict(lambda : [])
+    max_x = 0
+    max_y = 0
+    for x, line in enumerate(f.readlines()):
+        for y, c in enumerate(line):
+            if c != '.' and c != '\n':
+                antennas[c].append((x,y))
+            if y > max_y:
+                max_y = y
+        if x > max_x:
+            max_x = x
+    max_y -= 1
+    antinodes = set()
+    for val in antennas.values():
+        for el in val:
+            for other in val:
+                if el != other:
+                    diffx = (el[0] - other[0]) + el[0]
+                    diffy = (el[1] - other[1]) + el[1]
+                    if diffx < 0 or diffx > max_x or diffy < 0 or diffy > max_y:
+                        continue
+                    antinodes.add((diffx, diffy))
+    print(len(antinodes))
+    f.close()
+
+if __name__ == '__main__':
+    main()
